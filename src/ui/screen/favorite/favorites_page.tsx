563877b3b1d1b6502fd9cbd94e6aa323c46ebecd
arezz <davide.arezzi@edu.itspiemonte.it>
2025-01-21 17:40:15 +0100

    fix favorite page

--- src/ui/screen/favorite/favorites_page.tsx
+++ src/ui/screen/favorite/favorites_page.tsx
@@ -1,19 +1,64 @@
 import { Button, Text, View, FlatList } from 'react-native';
-import React  { useCallback, useEffect, useMemo } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { MainParamList, Screen } from '../../navigation/types';
 import { favoritesStyles } from './favorites_styles';
+import Card from '../../atoms/product/product.atom';
+import { useProducts } from '../hook/useProducts.facade';
 
 interface Props {
   navigation: NativeStackNavigationProp<MainParamList, Screen.Favorites>;
 }
 
 const FavoritesScreen = ({ navigation }: Props) => {
-  const { products}
+  const { products, favoriteIds,refreshProducts,loadFavorites, addFavorite} = useProducts();
+  
+  //**DATA **//
+  const favorites = useMemo(
+    () => products.filter((product) => favoriteIds.includes(product.id)),
+    [products, favoriteIds]
+  );
+  
+  // **CALLBACKS ** //
+  const renderItem = useCallback(
+    ({ item }) => (
+      <Card
+      product={item}
+      onAddFavorite={() => addFavorite(item)}
+      selected={favoriteIds.includes(item.id)}
+      />
+    ),
+    [addFavorite, favoriteIds]
+  );
+  
+  const ItemSeparatorComponent = useCallback(() => <View style={favoritesStyles.itemSeparator}></View>, []);
+  
+  //**USE EFFECT **//
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      console.log('Favoreite screen focused');
+      refreshProducts();
+      loadFavorites();
+    });
+
+    return unsubscribe;
+  }, [loadFavorites,navigation,refreshProducts]);
+
+
   return (
     <View style={favoritesStyles.container}>
-      <Text>Schermata favorites</Text>
-      <Button title={'Go to home'} onPress={() => navigation.navigate('Home')} />
+      {
+        favorites.length > 0 ? (
+          <FlatList
+          showsVerticalScrollIndicator={false}
+          data={favorites}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id.toString()}
+          ItemSeparatorComponent={ItemSeparatorComponent}
+          />
+        ) : (
+          <Text>Nofavorites yet</Text>
+        )} 
     </View>
   );
 };
